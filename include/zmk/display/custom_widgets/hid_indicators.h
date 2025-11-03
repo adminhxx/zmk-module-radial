051062a911afa8fc11152eab21a4c250a0b6d358
xuqingzhong <13588196563@163.com>
2025-11-03 17:52:39 +0800

    del-display

--- include/zmk/display/custom_widgets/hid_indicators.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <lvgl.h>
-
-lv_obj_t *lv_hid_indicators_create(lv_obj_t *parent, lv_style_t *container_style);
--- include/zmk/display/status_presenter.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <lvgl.h>
-
-void zmk_status_presenter_init(void);
-void zmk_status_presenter_dispatch(lv_obj_t *container, uint8_t depth);
